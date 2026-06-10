b2194ee0b816511ca8fad4e4b6338ab7cddb5218
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-06-10 16:23:28 +0100

    feat(timingWheel): refactor schedule method to use job object and consider priority and created time

--- src/timingWheel.js
+++ src/timingWheel.js
@@ -3,34 +3,53 @@ export class timingWheel {
     this.tickMs = tickMs;
     this.slots = slots;
     this.cursor = 0;
-    this.buckets = Array.from({ length: slots }, () => []);
+    this.buckets = Array.from({ length: slots }, () => ({
+      1: [],
+      2: [],
+      3: [],
+    }));
   }
 
   // put job in future slot
-  schedule(id, delayMs) {
-    const ticks = Math.max(1, Math.ceil(delayMs / this.tickMs)); //12
+  schedule(job) {
+    const { id, priority, scheduledAt, createdAt } = job;
+    const now = Date.now();
+    const oneHourMs = 60 * 60 * 1000;
+    const scheduledMs = scheduledAt ? new Date(scheduledAt).getTime() : now;
+    const createdAtMs = new Date(createdAt).getTime();
+    const delayMs = Math.max(1000, scheduledMs - now);
+
+    const ticks = Math.max(1, Math.ceil(delayMs / this.tickMs));
     const slot = (this.cursor + ticks) % this.slots;
     const rounds = Math.floor((ticks - 1) / this.slots);
-    this.buckets[slot].push({ id, rounds });
+    this.buckets[slot][priority].push({ id, rounds, createdAtMs });
   }
 
   // return runnable jobs
   tick() {
     this.cursor = (this.cursor + 1) % this.slots;
     const bucket = this.buckets[this.cursor];
-    const keep = [];
     const run = [];
 
-    for (const job of bucket) {
-      if (job.rounds > 0) {
-        job.rounds--;
-        keep.push(job);
-      } else {
-        run.push(job);
+    for (const priority in bucket) {
+      const keep = [];
+      const due = [];
+      for (const job of bucket[priority]) {
+        if (job.rounds > 0) {
+          job.rounds -= 1;
+          keep.push(job);
+        } else {
+          due.push(job);
+        }
       }
+
+      // consider created time when creating run
+      due.sort((a, b) => a.createdAtMs - b.createdAtMs);
+      run.push(...due);
+
+      bucket[priority] = keep;
     }
 
-    this.buckets[this.cursor] = keep;
     return run;
   }
 }
