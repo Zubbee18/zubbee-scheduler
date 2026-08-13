002ba3bdd3e4df7058be0a33d3790fbd4fa37d6b
Deborah Anyachukwu <108271834+zub-bee@users.noreply.github.com>
2026-08-13 22:57:05 +0900

    chore: use environment variable for SQLite database path

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 import Database from "better-sqlite3";
 
-const db = new Database("database.db");
+const filePath = process.env.SQLITE_PATH || "database.db";
+const db = new Database(filePath);
+
 db.pragma("journal_mode = WAL");
 db.pragma("busy_timeout = 5000");
 export default db;
